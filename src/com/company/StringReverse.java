809ec5ba940bcf0eb95d1b5d9eb2168dfacec63e
Andreas Nilsson <andreas.nilsson@kyh.se>
2020-09-17 09:49:47 +0200

    Added my own solution to reverse

---
+++ src/com/company/StringReverse.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class StringReverse {
+    public static String reverse(String in) {
+        String result = "";
+
+        for (int i=in.length()-1; i>=0; i--) {
+            result = result + in.charAt(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("Hello World"));
+    }
+}
