28b457c448b7e6fa4b8d87a5e4756e0246938d03
Andreas Nilsson <andreas.nilsson@kyh.se>
2020-09-17 09:18:02 +0200

    Post exercises for Strings and Files

---
+++ src/com/company/SearchlastString.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class SearchlastString {
+    public static void main(String[] args) {
+        String helloWorld = "Hello world, Hello Reader";
+
+        // Skriv ut vilket index den sista träffen på "Hello" har.
+        // Kolla i JavaDoc vad det finns för funktioner för detta.
+        // https://docs.oracle.com/en/java/javase/14/docs/api/java.base/java/lang/String.html
+    }
+}
