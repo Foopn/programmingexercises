9215cbef087f02c4f4aa6fea7ad945a91c77e002
David Sundelius <david.sundelius@gmail.com>
2020-09-18 09:50:45 +0200

    Added writefile to lecture 4

--- src/kyh/lectures/Lecture4.java
+++ src/kyh/lectures/Lecture4.java
@@ -1,12 +1,23 @@
 package kyh.lectures;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Lecture4 {
 
+    public static void printFile(String content) {
+        File file = new File("CJL.txt");
+        try {
+            FileWriter fw = new FileWriter(file);
+            fw.write(content);
+            fw.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-    public static void main(String[] args) {
+    public static void readFile() {
         File file = new File("CJL.txt");
         try {
             Scanner sc = new Scanner(file);
@@ -16,5 +27,9 @@ public class Lecture4 {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        printFile("Varför är den gode dum?");
     }
 }
