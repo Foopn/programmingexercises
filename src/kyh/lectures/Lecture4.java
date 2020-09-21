8ec0cbe88520c8a3d5c27332f48ba545df660f39
David Sundelius <david.sundelius@gmail.com>
2020-09-21 11:19:26 +0200

    Added last code for Lecture 4

--- src/kyh/lectures/Lecture4.java
+++ src/kyh/lectures/Lecture4.java
@@ -1,10 +1,46 @@
 package kyh.lectures;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Lecture4 {
+
+    public static void printPathIfWordInFile(File f, String word) {
+        try {
+            Scanner sc = new Scanner(f);
+            while(sc.hasNext()) {
+                if(sc.next().equals(word)) {
+                    System.out.println(f.getCanonicalPath());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void traverseTree(File dir, String word) {
+        for(File f: dir.listFiles()) {
+            if(f.isDirectory()) {
+                traverseTree(f, word);
+            } else {
+                printPathIfWordInFile(f, word);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Välkommen till FileCrawler!");
+        System.out.println("Skriv in ett sökord så listar jag alla filer som innehåller det ordet");
+        System.out.print("Sökord: ");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        traverseTree(new File(".") ,word);
+    }
+
+
 
     public static void printFile(String content) {
         File file = new File("CJL.txt");
@@ -37,8 +73,4 @@ public class Lecture4 {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(4));
-        //printFile("Varför är den gode dum?");
-    }
 }
