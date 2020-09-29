5511b5bfe1c0c80d9adcbd5904f0569fcf0fc9c4
David Sundelius <david.sundelius@gmail.com>
2020-09-29 09:34:57 +0200

    Added implementation of angular triangle

--- src/kyh/lectures/lecture7/Main.java
+++ src/kyh/lectures/lecture7/Main.java
@@ -1,2 +1,8 @@
-package kyh.lectures.lecture7;public class Main {
+package kyh.lectures.lecture7;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape rect = new Triangle(5, 6, 7);
+        System.out.println(rect.getArea() + " " + rect.getCircumference());
+    }
 }
