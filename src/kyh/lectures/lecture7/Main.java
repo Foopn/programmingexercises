e6fb0ea0b16964dec043464b834994f8a45855b6
David Sundelius <david.sundelius@gmail.com>
2020-09-29 10:01:22 +0200

    Added example on Math.random()

--- src/kyh/lectures/lecture7/Main.java
+++ src/kyh/lectures/lecture7/Main.java
@@ -2,7 +2,9 @@ package kyh.lectures.lecture7;
 
 public class Main {
     public static void main(String[] args) {
-        Shape rect = new Triangle(5, 6, 7);
-        System.out.println(rect.getArea() + " " + rect.getCircumference());
+        //Shape rect = new Triangle(5, 6, 7);
+        //System.out.println(rect.getArea() + " " + rect.getCircumference());
+        long diceRoll = Math.round(Math.random()*5 + 1);
+        System.out.println(diceRoll);
     }
 }
