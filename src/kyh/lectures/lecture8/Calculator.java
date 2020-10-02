40ced3263a783b224c7ecdea7ad106fe65a75d2a
David Sundelius <david.sundelius@gmail.com>
2020-10-02 12:54:38 +0200

    Added Multiplication and task to do during lecture 8

--- src/kyh/lectures/lecture8/Calculator.java
+++ src/kyh/lectures/lecture8/Calculator.java
@@ -7,18 +7,32 @@ public class Calculator {
         return a+b;
     }
 
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
     public static void main(String[] args) {
         int a, b;
         String method;
         Calculator calc = new Calculator();
         System.out.println("Welcome to the Calculator!");
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()) {
+
+        //Lägg till Modulo (%), Division (/), Max(max)
+        while (sc.hasNext()) {
             a = sc.nextInt();
             method = sc.next();
             b = sc.nextInt();
-            if(method.equals("+")) {
-                System.out.println("The answer is: " + calc.add(a,b));
+            if (method.equals("+")) {
+                System.out.println("The answer is: " + calc.add(a, b));
+            } else if (method.equals("-")) {
+                System.out.println("The answer is: " + calc.subtract(a, b));
+            } else if (method.equals("*")) {
+                System.out.println("The answer is: " + calc.multiply(a, b));
             } else {
                 System.out.println("The method of calculation is not supported yet");
             }
--- src/kyh/lectures/lecture8/CalculatorTests.java
+++ src/kyh/lectures/lecture8/CalculatorTests.java
@@ -1,9 +1,9 @@
 package kyh.lectures.lecture8;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Testing Calculator")
 public class CalculatorTests {
@@ -15,5 +15,22 @@ public class CalculatorTests {
         assertEquals(3, calculator.add(1, 2));
         assertEquals(5, calculator.add(-5, 10));
         assertEquals(1400000, calculator.add(700000, 700000));
+    }
+
+    @Test
+    @DisplayName("Testing that subtraction works")
+    void subtraction() {
+        assertEquals(-1, calculator.subtract(1, 2));
+        assertEquals(-15, calculator.subtract(-5, 10));
+        assertEquals(0, calculator.subtract(700000, 700000));
+        assertNotEquals(-15, calculator.subtract(-15,-1));
+    }
+
+    @Test
+    @DisplayName("Testing that multiplication works")
+    void multiplication() {
+        assertEquals(2, calculator.multiply(1, 2));
+        assertEquals(-35, calculator.multiply(-7, 5));
+        assertEquals(20000, calculator.multiply(200, 100));
     }
 }
