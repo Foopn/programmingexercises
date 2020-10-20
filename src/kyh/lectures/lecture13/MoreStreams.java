a39e2f594b84cfc9d9874463f694b00e796a8486
Andreas Nilsson <andreas.nilsson@kyh.se>
2020-10-20 09:27:54 +0200

    Uppdaterat exempel på streams

--- src/kyh/lectures/lecture13/MoreStreams.java
+++ src/kyh/lectures/lecture13/MoreStreams.java
@@ -1,5 +1,6 @@
 package kyh.lectures.lecture13;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,9 +11,19 @@ public class MoreStreams {
         return a + b;
     }
 
+    interface MyAdd {
+        int func(int x, int y);
+    }
 
     public static void main(String[] args) {
         // Repetition: Gör om add() till en lambda-funktion
+        MyAdd addFunc = (a, b) -> {
+            return a + b;
+        };
+
+        int summa = addFunc.func(8, 21);
+
+        System.out.println("Summan är " + summa);
 
 
         /*
@@ -28,9 +39,9 @@ public class MoreStreams {
         // Alternativt sätt att skapa en lista
         List<Integer> array = Arrays.asList(1, 2, 6, 3, 5, 4);
 
-        /*
-         * Exempel på filter, sorted och collect
-         */
+
+        // Exempel på filter, sorted och collect
+
         List<Integer> newArray =  // Spara resultatet i en ny variabel
                 array.stream()
                 .filter(i -> i > 3)  // Hitta alla heltal större än 3
@@ -42,6 +53,7 @@ public class MoreStreams {
         /*
          * Exempel: Räkna element
          */
+
         long evenCount =
                 array.stream()
                 .filter(i -> i % 2 == 0)  // Hitta alla jämna tal
@@ -52,8 +64,9 @@ public class MoreStreams {
         /*
          * Exempel: Reduce - Summera ints
          */
-        Integer reduceSum = array.stream()
-                .reduce(0, (a, b) -> a + b);
+
+        int reduceSum = array.stream()
+                .reduce(-200, (a, b) -> a + b);
         System.out.println("Reduce() med summering ger: " + reduceSum);
 
         // Streamen är: 1, 2, 6, 3, 5, 4
@@ -66,14 +79,20 @@ public class MoreStreams {
         //    3 + 6 = 9
         //    osv...
 
+        
         /*
          * Exempel: Reduce - Bygg ihop strängar
          */
 
         List<String> letters = Arrays.asList("a", "b", "c", "d", "e");
+        ArrayList<String> lettersarr = new ArrayList<>();
+        lettersarr.add("a");
+
         String reducedString = letters
                 .stream()
-                .reduce("", (res, element) -> res + element);
+                .reduce("Start-värde... ", (s, element) -> s + element.toUpperCase());
         System.out.println("Reduce() med strängar ger: " + reducedString);
+
+
     }
 }
